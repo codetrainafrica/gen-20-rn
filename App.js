fac99f00727c0f9c01de4e089ac943ce9c588da0
Tony <toneyobaze@gmail.com>
2022-05-04 13:34:26 +0000

    render basic components and styles

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  Button,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import Cat from "./assets/cat.jpeg";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 30, fontWeight: "bold" }}>Hello</Text>
+      <Image
+        // source={Cat}
+        // source={require("./assets/cat.jpeg")}
+        source={{
+          uri: "https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80",
+        }}
+        style={{ height: 100, width: 100 }}
+      />
+
+      <Button title="Click me" />
+
+      <TouchableOpacity
+        style={{
+          height: 200,
+          width: 200,
+          backgroundColor: "tomato",
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>Click me</Text>
+      </TouchableOpacity>
+
+      <TextInput />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "blue",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
