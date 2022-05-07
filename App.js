be81528e53b33409f764330ced6a040267d22ae8
Tony <toneyobaze@gmail.com>
2022-05-07 14:11:12 +0000

    use basic components

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import car from "./assets/car.jpeg";
 
-export default function App() {
+const App = () => {
+  const [name, setName] = useState("");
+
+  const handleChange = (text) => {
+    setName(text);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <TouchableOpacity>
+        <Text style={styles.title}>Hello</Text>
+        <Text style={{ fontSize: 40, fontWeight: "bold" }}>
+          Lorem ipsum dolor
+        </Text>
+
+        <View style={{ height: 20, backgroundColor: "red" }}></View>
+        {/* <Image source={car} style={{ height: 100, width: 100 }} /> */}
+        {/* <Image
+        source={require("./assets/car.jpeg")}
+        style={{ height: 100, width: 100 }}
+      /> */}
+        <Image
+          source={{
+            uri: "https://images.unsplash.com/photo-1556189250-72ba954cfc2b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          }}
+          style={{ height: 100, width: 100 }}
+        />
+      </TouchableOpacity>
+
+      {/* <Button title="CLick me" /> */}
+
+      <TouchableOpacity style>
+        <Text>Click me</Text>
+      </TouchableOpacity>
+
+      <TextInput value={name} onChangeText={handleChange} />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+
+  title: {
+    color: "green",
   },
 });
+
+export default App;
